60091377e573239f31c28a0c80300ec6efc09693
Victor Kadiata <vickadiata@gmail.com>
2022-04-26 22:49:38 +0100

    feat: add CRUD functionality for hospitals

---
+++ hospitals/urls.py
@@ -0,0 +1,9 @@
+from django.urls import URLPattern
+from rest_framework.routers import DefaultRouter
+from .views import HospitalViewSet
+
+router = DefaultRouter()
+
+router.register(prefix="api/v1/hospitals", viewset=HospitalViewSet, basename="hospital")
+
+urlpatterns = router.urls
--- hospitals/views.py
+++ hospitals/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Hospital
+from .serializers import HospitalSerializer
 
 # Create your views here.
+class HospitalViewSet(viewsets.ModelViewSet):
+    queryset = Hospital.objects.all()
+    serializer_class = HospitalSerializer
