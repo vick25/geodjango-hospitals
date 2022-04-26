1d209b6029e1f17e82712389f40ea4427617b6bd
Victor Kadiata <vickadiata@gmail.com>
2022-04-26 23:12:29 +0100

    feat: get area of provinces

--- boundaries/serializers.py
+++ boundaries/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework_gis.serializers import GeoFeatureModelSerializer
+from rest_framework import serializers
 from .models import Boundary
 
 
 class BoundarySerializer(GeoFeatureModelSerializer):
+    area = serializers.CharField(max_length=100)
+
     class Meta:
         model = Boundary
         geo_field = "mpoly"
--- boundaries/views.py
+++ boundaries/views.py
@@ -4,7 +4,13 @@ from rest_framework import viewsets
 from .models import Boundary
 from .serializers import BoundarySerializer
 
+from django.contrib.gis.db.models.functions import Area
+
 # Create your views here.
 class BoundaryViewset(viewsets.ModelViewSet):
     queryset = Boundary.objects.all()
     serializer_class = BoundarySerializer
+
+    def get_queryset(self):
+        boundary_area = Boundary.objects.annotate(area=Area("mpoly")).order_by("area")
+        return boundary_area
