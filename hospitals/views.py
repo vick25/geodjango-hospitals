0c72f83840c8045dc2e37b1e923a7eee50f2e988
Victor Kadiata <vickadiata@gmail.com>
2022-04-27 12:10:11 +0100

    feat: get beds capacity per province

--- hospitals/views.py
+++ hospitals/views.py
@@ -16,3 +16,10 @@ class HospitalViewSet(viewsets.ModelViewSet):
     def total_bed_capacity(self, request):
         bed_capactiy = Hospital.objects.aggregate(bed_capactiy=Sum("beds"))
         return Response(bed_capactiy)
+
+    @action(detail=False, methods=["get"])
+    def province_beds_capacity(self, request):
+        province_bed_capacity = Hospital.objects.values("province_name").annotate(
+            bed_capacity=Sum("beds")
+        )
+        return Response(province_bed_capacity)
